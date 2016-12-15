35aada82400d415fcda8bbc303d4c927a8e27556
NicolePell <pellicena@gmail.com>
2016-12-15 14:47:21 +0000

    add call to weather api and reducer to handle action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = "0cfffcd3d9a53a7713ba23d57d1c6750";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city, country = "gb") {
+  const url = `${ROOT_URL}&q=${city},${country}`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
